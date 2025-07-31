107b8438d87ec99c2ef214e9bd9f4201a28e3a7d
BALA SURYA S <balasuryas2006@gmail.com>
2025-07-31 11:51:17 +0530

    sum of even number 1.java

---
+++ sum of even number 1.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int m=sc.nextInt();
+        int sum=0;
+        
+        if(m>n && n>0 && n<30 && m>0 && m<30){
+                for(int i=n;i<m;i++){
+                    if(i%2==0){
+                        sum+=i;
+                    }
+                }
+            System.out.println(sum);
+            }
+        
+        else{
+            System.out.println("Invalid Input");
+        }
+    }
+}
