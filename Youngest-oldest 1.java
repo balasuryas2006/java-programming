745d3b56ba6da3f0bae378f95767ace18e9dbf39
BALA SURYA S <balasuryas2006@gmail.com>
2025-08-01 09:10:19 +0530

    Youngest-oldest 1.java

---
+++ Youngest-oldest 1.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int arr[]=new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        Arrays.sort(arr);
+        System.out.println("Youngest="+arr[0]);
+        System.out.println("Oldest="+arr[n-1]);
+        
+    }
+}
